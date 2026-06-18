1c9809e20285d94a51eb86de89dbd46338598af2
Ahmed-TechCampus <48793305+Ahmed-TechCampus@users.noreply.github.com>
2026-06-18 21:49:44 +0300

    Create mt5_connector.py

---
+++ GoldBot-Pro/core/mt5_connector.py
@@ -0,0 +1,21 @@
+class MT5Connector:
+
+    def connect()
+
+    def disconnect()
+
+    def reconnect()
+
+    def account_info()
+
+    def symbol_info()
+
+    def get_tick()
+
+    def get_rates()
+
+    def positions()
+
+    def send_order()
+
+    def close_position()
